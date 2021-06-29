82bd523156539236afc0c3b9117eb9b3cac6216c
RhinolamerSonwar <rhinolamer@gmail.com>
2021-06-29 16:57:06 +0300

    admin part done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,94 @@
+// import React from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { BrowserRouter, Route, Link } from "react-router-dom";
+// import { signOut } from "./actions/userActions";
+// import PlaceOrderScreen from "./reducers/PlaceOrderScreen";
+
+// import CartScreen from "./Screens/CartScreen";
+// import HomeScreen from "./Screens/HomeScreen";
+// import OrderHistoryScreen from "./Screens/OrderHistoryScreen";
+// import OrderScreen from "./Screens/OrderScreen";
+// import PaymentMethodScreen from "./Screens/PaymentMethodScreen";
+// import ProductScreen from "./Screens/ProductScreen";
+// import ProfileScreen from "./Screens/ProfileScreen";
+// import RegisterScreen from "./Screens/RegisterScreen";
+// import ShippingAddressScreen from "./Screens/ShippingAddressScreen";
+
+// import SigninScreen from "./Screens/SigninScreen";
+
+// function App() {
+//   const cart = useSelector((state) => state.cart);
+//   const { cartItems } = cart;
+//   const userSignin = useSelector((state) => state.userSignin);
+//   const { userInfo } = userSignin;
+//   const dispatch = useDispatch();
+//   const signoutHandler = () => {
+//     dispatch(signOut());
+//   };
+//   return (
+//     <BrowserRouter>
+//       <div className="grid-container">
+//         <header className="row">
+//           <div>
+//             <Link className="brand" to="/">
+//               amazona
+//             </Link>
+//           </div>
+//           <div>
+//             <Link to="/cart">
+//               Cart
+//               {cartItems.length > 0 && (
+//                 <span className="badge">{cartItems.length}</span>
+//               )}
+//             </Link>
+//             {userInfo ? (
+//               <div className="dropdown">
+//                 <Link to="#">
+//                   {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
+//                 </Link>
+//                 <ul className="dropdown-content">
+//                   <li>
+//                     <Link to="/profile">User Profile</Link>
+//                   </li>
+//                   <li>
+//                     <Link to="/orderhistory">Order History</Link>
+//                   </li>
+//                   <li>
+//                     <Link to="/signout" onClick={signoutHandler}>
+//                       Sign Out
+//                     </Link>
+//                   </li>
+//                 </ul>
+//               </div>
+//             ) : (
+//               <Link to="/signin">Sign In</Link>
+//             )}
+//           </div>
+//         </header>
+//         <main>
+//           <Route path="/cart/:id?" component={CartScreen}></Route>
+//           <Route path="/product/:id" component={ProductScreen}></Route>
+//           <Route path="/signin" component={SigninScreen}></Route>
+//           <Route path="/register" component={RegisterScreen}></Route>
+//           <Route path="/shipping" component={ShippingAddressScreen}></Route>
+//           <Route path="/payment" component={PaymentMethodScreen}></Route>
+//           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+//           <Route path="/order/:id" component={OrderScreen}></Route>
+//           <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+//           <Route path="/profile" component={ProfileScreen}></Route>
+//           <Route path="/" component={HomeScreen} exact></Route>
+//         </main>
+//         <footer className="row center">
+//           {" "}
+//           All right reserved @RhinolamerSonwar
+//         </footer>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route, Link } from "react-router-dom";
@@ -13,6 +104,7 @@ import ProductScreen from "./Screens/ProductScreen";
 import ProfileScreen from "./Screens/ProfileScreen";
 import RegisterScreen from "./Screens/RegisterScreen";
 import ShippingAddressScreen from "./Screens/ShippingAddressScreen";
+import PrivateRoute from "./components/PrivateRoute";
 
 import SigninScreen from "./Screens/SigninScreen";
 
@@ -54,7 +146,7 @@ function App() {
                     <Link to="/orderhistory">Order History</Link>
                   </li>
                   <li>
-                    <Link to="#signout" onClick={signoutHandler}>
+                    <Link to="#signOut" onClick={signoutHandler}>
                       Sign Out
                     </Link>
                   </li>
@@ -62,6 +154,27 @@ function App() {
               </div>
             ) : (
               <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist">Orders</Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
             )}
           </div>
         </header>
@@ -75,13 +188,13 @@ function App() {
           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
           <Route path="/order/:id" component={OrderScreen}></Route>
           <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
-          <Route path="/profile" component={ProfileScreen}></Route>
+          <PrivateRoute
+            path="/profile"
+            component={ProfileScreen}
+          ></PrivateRoute>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
-        <footer className="row center">
-          {" "}
-          All right reserved @RhinolamerSonwar
-        </footer>
+        <footer className="row center">All right reserved</footer>
       </div>
     </BrowserRouter>
   );
